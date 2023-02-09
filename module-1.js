f10dd7574f91a88105b5da70c84c80fe8db0f1c2
Bence A. Tóth <tothab@gmail.com>
2023-02-09 11:13:29 +0100

    Added modules example

---
+++ module-1.js
@@ -0,0 +1,9 @@
+let myVariable = 10;
+
+let myDefaultVariable = 20;
+
+let yetAnotherVariable = 30;
+
+export { myVariable, yetAnotherVariable };
+
+export default myDefaultVariable;
---
+++ module-2.js
@@ -0,0 +1,9 @@
+import { myVariable, yetAnotherVariable } from "./module-1.js";
+
+import myWhatever from "./module-1.js";
+
+console.log(myVariable);
+
+console.log(myWhatever);
+
+console.log(yetAnotherVariable);
